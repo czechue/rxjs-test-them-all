89d628ee1932e8cce94855f0553d210663731499
Michal Lester <michal.lester@allegro.pl>
2018-11-12 09:33:17 +0100

    old tests refactor

--- src/scripts/__test__/examples.test.js
+++ src/scripts/__test__/examples.test.js
@@ -1,77 +1,78 @@
 import '../examples/ex4_implement-map';
-// const sum = require();
+import newReleases from '../mocks/data';
 import { traversingArray, traversingWithForEach } from '../examples/ex1_ex2';
 import projectingArrays from '../examples/ex3-projecting-arrays';
-import newReleases from '../mocks/data';
 
 const names = ['Ben', 'Jafar', 'Matt', 'Priya', 'Brian'];
 
-// ex1
-test('traversingArray', () => {
-  const result = traversingArray(names);
-  expect(result).toEqual(names);
-});
-
-//ex2
-test('traversingWithForEach', () => {
-  const result = traversingWithForEach(names);
-  expect(result).toEqual(names);
-});
-
-// ex3
-test('projectingArrays', () => {
-  const resultArr = [
-    {
-      id: 70111470,
-      title: 'Die Hard',
-    },
-    {
-      id: 654356453,
-      title: 'Bad Boys',
-    },
-    {
-      id: 65432445,
-      title: 'The Chamber',
-    },
-    {
-      id: 675465,
-      title: 'Fracture',
-    },
-  ];
-  const result = projectingArrays(newReleases);
-  expect(result).toEqual(resultArr);
-});
-
-// ex4
-test('implementMap', () => {
-  const arr = [1, 2, 3];
-  const res = arr.myMap(i => i + 1);
-  expect(res).toEqual([2, 3, 4]);
-});
-
-// ex5
-test('projectingArrays', () => {
-  const resultArr = [
-    {
-      id: 70111470,
-      title: 'Die Hard',
-    },
-    {
-      id: 654356453,
-      title: 'Bad Boys',
-    },
-    {
-      id: 65432445,
-      title: 'The Chamber',
-    },
-    {
-      id: 675465,
-      title: 'Fracture',
-    },
-  ];
-
-  const result = newReleases.myMap(video => {
-    return { id: video.id, title: video.title };
+describe('ex1 - ex5', () => {
+  // ex1
+  test('traversingArray', () => {
+    const result = traversingArray(names);
+    expect(result).toEqual(names);
   });
-  expect(result).toEqual(resultArr);
+
+  //ex2
+  test('traversingWithForEach', () => {
+    const result = traversingWithForEach(names);
+    expect(result).toEqual(names);
+  });
+
+  // ex3
+  test('projectingArrays', () => {
+    const resultArr = [
+      {
+        id: 70111470,
+        title: 'Die Hard',
+      },
+      {
+        id: 654356453,
+        title: 'Bad Boys',
+      },
+      {
+        id: 65432445,
+        title: 'The Chamber',
+      },
+      {
+        id: 675465,
+        title: 'Fracture',
+      },
+    ];
+    const result = projectingArrays(newReleases);
+    expect(result).toEqual(resultArr);
+  });
+
+  // ex4
+  test('implementMap', () => {
+    const arr = [1, 2, 3];
+    const res = arr.myMap(i => i + 1);
+    expect(res).toEqual([2, 3, 4]);
+  });
+
+  // ex5
+  test('projectingArrays', () => {
+    const resultArr = [
+      {
+        id: 70111470,
+        title: 'Die Hard',
+      },
+      {
+        id: 654356453,
+        title: 'Bad Boys',
+      },
+      {
+        id: 65432445,
+        title: 'The Chamber',
+      },
+      {
+        id: 675465,
+        title: 'Fracture',
+      },
+    ];
+
+    const result = newReleases.myMap(video => {
+      return { id: video.id, title: video.title };
+    });
+    expect(result).toEqual(resultArr);
+  });
 });
