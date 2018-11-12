5b1c2dd7ce76c6637d0a0f72b97aab12fea325ea
Michal Lester <michal.lester@allegro.pl>
2018-11-12 09:28:40 +0100

    finished 5ec - map()

--- src/scripts/__test__/examples.test.js
+++ src/scripts/__test__/examples.test.js
@@ -1,4 +1,4 @@
-import '../examples/ex4_implement-map'
+import '../examples/ex4_implement-map';
 // const sum = require();
 import { traversingArray, traversingWithForEach } from '../examples/ex1_ex2';
 import projectingArrays from '../examples/ex3-projecting-arrays';
@@ -44,7 +44,34 @@ test('projectingArrays', () => {
 
 // ex4
 test('implementMap', () => {
-  const arr = [1,2,3];
-  const res = arr.myMap((i) => i + 1);
-  expect(res).toEqual([2,3,4])
+  const arr = [1, 2, 3];
+  const res = arr.myMap(i => i + 1);
+  expect(res).toEqual([2, 3, 4]);
+});
+
+// ex5
+test('projectingArrays', () => {
+  const resultArr = [
+    {
+      id: 70111470,
+      title: 'Die Hard',
+    },
+    {
+      id: 654356453,
+      title: 'Bad Boys',
+    },
+    {
+      id: 65432445,
+      title: 'The Chamber',
+    },
+    {
+      id: 675465,
+      title: 'Fracture',
+    },
+  ];
+
+  const result = newReleases.myMap(video => {
+    return { id: video.id, title: video.title };
+  });
+  expect(result).toEqual(resultArr);
 });
